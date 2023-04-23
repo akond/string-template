93ad8328193d568f50f5fad7f31515acfa90f902
Toropenko Sergey <i.m.just.a.humble.tester@gmail.com>
2023-04-23 17:03:07 +0300

    update

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject com.github.akond/string-template "0.0.1-SNAPSHOT"
+
+	:description "A Clojure wrapper around StringTemplate"
+
+	:dependencies [[org.clojure/clojure "1.11.1"]
+				   [org.antlr/stringtemplate "4.0.2"]]
+
+	:license {:name         "Apache License, Version 2.0"
+			  :url          "https://www.apache.org/licenses/LICENSE-2.0"
+			  :distribution :repo}
+
+	:repositories {"clojars" {:url "https://clojars.org/repo"}}
+
+	:repl-options {:init-ns string-template.core})
---
+++ src/string_template/core.clj
@@ -0,0 +1,78 @@
+(ns string-template.core
+	(:import
+		(clojure.lang Associative ILookup IPersistentCollection IPersistentMap)
+		(java.io File)
+		(java.security InvalidKeyException)
+		(org.stringtemplate.v4 ST STGroupFile STGroupString)))
+
+; os described at https://github.com/antlr/stringtemplate4/blob/master/doc/cheatsheet.md
+(def reserved (->>
+				  '(true, false, import, default, key, group, implements, first, last, rest, trunc, strip, trim, length,
+					   strlen, reverse, if, else, elseif, endif, delimiters)
+				  (map str)
+				  (into #{})))
+
+(defn stringify [k]
+	(cond-> k
+		(or (keyword? k) (symbol? k)) name))
+
+(deftype StringTemplate [template]
+	Object
+	(toString [_]
+		(.render template))
+
+	IPersistentCollection
+	(cons [this a]
+		(reduce (partial apply assoc) this a)
+		this)
+
+	(empty [this]
+		(doseq [k (keys (.getAttributes template))]
+			(.remove template k))
+		this)
+
+	IPersistentMap
+	(without [this k]
+		(.remove template (stringify k))
+		this)
+
+	Associative
+	(assoc [this k v]
+		(let [k (stringify k)]
+			(when (contains? reserved k)
+				(throw (InvalidKeyException. (format "'%s' is a reserved name." k))))
+			(.add template k (if (map? v) (clojure.walk/stringify-keys v) v)))
+		this))
+
+(defmulti template class)
+
+(defmethod template :default [s]
+	(->StringTemplate (ST. s)))
+
+(defmethod template ST [s]
+	(->StringTemplate s))
+
+(defmethod template File [s]
+	(->StringTemplate (ST. (slurp s))))
+
+(defmethod print-method StringTemplate [t w]
+	(->> t str (.write w)))
+
+(deftype StringTemplateGroup [g]
+	ILookup
+	(valAt [this k]
+		(template (.getInstanceOf g (stringify k))))
+	(valAt [this k none]
+		(let [k' (stringify k)]
+			(if (.isDefined g k')
+				(template (.getInstanceOf g k'))
+				none))))
+
+(defmulti group class)
+
+(defmethod group :default [s]
+	(->StringTemplateGroup (STGroupString. s)))
+
+(defmethod group File [s]
+	(let [path (str (.getAbsoluteFile s))]
+		(->StringTemplateGroup (STGroupFile. path))))
