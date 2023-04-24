edf088fb1bfb0a37e889697860097cc31e5c6a42
Toropenko Sergey <i.m.just.a.humble.tester@gmail.com>
2023-04-24 10:34:36 +0300

    Minor fixes

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
-(defproject com.github.akond/string-template "0.0.3"
+(defproject com.github.akond/string-template "0.0.4"
 
 	:description "A Clojure wrapper around StringTemplate"
 
 	:url "https://github.com/akond/string-template"
 
-	:dependencies [[org.clojure/clojure "1.11.1"]
-				   [org.antlr/stringtemplate "4.0.2"]]
+	:dependencies [[org.antlr/stringtemplate "4.0.2"]]
 
 	:license {:name         "Apache License, Version 2.0"
 			  :url          "https://www.apache.org/licenses/LICENSE-2.0"
 			  :distribution :repo}
+
+	:profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]]}}
 
 	:repositories {"clojars" {:url "https://clojars.org/repo"}}
 
